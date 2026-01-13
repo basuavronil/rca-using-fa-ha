1299a57ec394c4f9f5ee74f5a5775fa8477f2a93
basuavronil <basuavronil65@gmail.com>
2026-01-13 22:36:37 +0530

    Added Test Bench

--- tb.v
+++ tb.v
@@ -2,6 +2,18 @@ module tb();
   reg [3:0] a,b;
   reg cin;
   wire [3:0] s;
-  wire c;
+  wire cout;
   //instanciate the design under test 
-  fa
+  rca dut(.a(a), .b(b), .cin(cin), .s(s), .cout(cout));
+  initial begin
+    $monitor (" %time | a=%0b  b=%0b  cin=%0b | cout=0%b  s=0%b", $time, a, b, cin, cout, s);
+
+    repeat (100) begin
+      {a,b,cin} = {a,b,cin} + 1;
+    end
+    $finish;
+  end
+endmodule
+
+      
+  
